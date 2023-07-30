f63443b4d4c3c9828576e438bbfeae3e3cb5fce4
Jrorem <joerorem@gmail.com>
2023-07-30 18:45:04 -0500

    added more acceptance criteria

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,12 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+import Row from 'react-bootstrap/Row';
+import concert from '../assets/concert.png';
+import pumped from '../assets/pumped.png';
+import quiz from '../assets/quiz.png';
+import weather from '../assets/weather.png';
+import password from '../assets/password.png';
+import schedule from '../assets/schedule.png';
+
+
 export default function Portfolio () {
     return <div id="portfolio">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={concert} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
             <Card.Title>Concerts Near Me</Card.Title>
             <Card.Text>
@@ -19,12 +37,17 @@ export default function Portfolio () {
         </div>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={weather} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
             <Card.Title>Weather Dashboard</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              An application to check the current weather and 5 day forecast for a city.
             </Card.Text>
             <Button
             href="https://jrorem.github.io/weather-dashboard/" variant="primary">View Project</Button>
@@ -33,53 +56,77 @@ export default function Portfolio () {
         </div>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={password} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
-            <Card.Title>Pumped Up</Card.Title>
+            <Card.Title>Password Generator</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              An application to randomly generate a secure password using parameters of your choice.
             </Card.Text>
-            <Button variant="primary">View Project</Button>
+            <Button 
+            href="https://jrorem.github.io/03-javascript-challenge/" variant="primary">View Project</Button>
           </Card.Body>
         </Card>
         </div>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={pumped} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Get Pumped</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A web application where a user can log in, search workouts and create workout routines.
             </Card.Text>
-            <Button variant="primary">View Project</Button>
+            <Button 
+            href="https://lit-atoll-87155-fc436e3971a9.herokuapp.com/" variant="primary">View Project</Button>
           </Card.Body>
         </Card>
         </div>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={schedule} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Event Scheduler</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A web application to create events on a calender. Color coded to denote timing of events.
             </Card.Text>
-            <Button variant="primary">View Project</Button>
+            <Button 
+            href="https://jrorem.github.io/day-scheduler/" variant="primary">View Project</Button>
           </Card.Body>
         </Card>
         </div>
         <div class="app">
         <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
+        {/* <Container >
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src={quiz} rounded />
+        </Col>
+        </Row>
+        </Container> */}
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>Coding Quiz</Card.Title>
             <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
+              A web application quiz to test your JavaScript knowledge and save high schores
             </Card.Text>
-            <Button variant="primary">View Project</Button>
+            <Button 
+            href="https://jrorem.github.io/Coding-Quiz/" variant="primary">View Project</Button>
           </Card.Body>
         </Card>
         </div>
